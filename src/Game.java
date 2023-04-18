0e8fdaa38f2f75f8e8dbe3f52a9276bd271854e5
dzhusupov-i-auca-2022 <add131115@gmail.com>
2023-04-19 00:33:16 +0200

    THE FINAL VERSION

--- src/Game.java
+++ src/Game.java
@@ -4,19 +4,17 @@ public class Game {
     public static Scanner scn = new Scanner(System.in);
 
     final static int MAX = 10;
-    public static char[][] user;
-    public static char[][] copy;
-    public static char[][] op;
-    public static int UserShips, CompShips;
+    private static char[][] user;
+    private static char[][] copy;
+    private static char[][] op;
+    private static int UserShips, CompShips;
+    public static int stat;
 
-    //Start and Set up the game
-    public static void startTheGame() {
-
-        System.out.println("~~~~~~~~~~~~~~~THE BATTLESHIPS~~~~~~~~~~~~~~~~\n" +
-                "-----------THE START OF THE GAME--------------\n");
-        user = new char[10][10];
-        op = new char[10][10];
-        copy = new char[10][10];
+    public Game(){
+        user = new char[MAX][MAX];
+        op = new char[MAX][MAX];
+        copy = new char[MAX][MAX];
+        stat = 0;
         UserShips = 24;
         CompShips = 24;
         for (int i = 0; i < MAX; i++) {
@@ -26,6 +24,10 @@ public class Game {
                 copy[i][j] = 'o';
             }
         }
+    }
+
+    //Start and Set up the game
+    public static void startTheGame() {
         inputUser();
         setCompShips();
         while (UserShips > 0 && CompShips > 0) {
@@ -46,86 +48,26 @@ public class Game {
                         "THE OPP : " + (24 - UserShips) + " YOUR : " + (24 - CompShips));
             }
         }
-        System.out.println(UserShips > CompShips ? "~~~~~~~~~~~~~!!!YOU WON!!!~~~~~~~~~~~~~~~~" : "~~~~~~~~~~~YOU LOSE~~~~~~~~~~");
+        if (UserShips > CompShips ){
+            System.out.println("~~~~~~~~~~~~~!!!YOU WON!!!~~~~~~~~~~~~~~~~");
+            stat++;
+        }else
+        {
+            System.out.println("~~~~~~~~~~~YOU LOSE~~~~~~~~~~");
+
+        }
         System.out.println("~~~~~~~~~~~~~~~THE END OF THE GAME~~~~~~~~~~~~~~~~");
-
     }
 
-    //Printing the board
-
-
-    private static void print(char[][] board) {
-        for (int i = 0; i < MAX; i++) {
-            System.out.printf(" %d", i);
-            for (int j = 0; j < MAX; j++) {
-                System.out.print(" " + board[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.print("-- A B C D E F G H I J\n");
-    }
-
-    // Input the coordinates of the hit by User
-    public static boolean inputHit() {
-        System.out.print("\nPlease, write down the coordinates of the hit ('0 A'): ");
-        String coorN = scn.next();
-        String coorL = scn.next();
-        int x = coorN.charAt(0) - 48;
-        int y = coorL.charAt(0) - 65;
-        try {
-            if (isShot(x, y, op)) {
-                op[x][y] = 'X';
-                copy[x][y] = 'X';
-                CompShips--;
-                System.out.println("You got it! Hit again");
-                return true;
-
-            } else if (op[x][y] == 'o') {
-                System.out.println("You missed :(");
-                copy[x][y] = 'M';
-                return false;
-            } else return true;
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("The out of board! Try again!");
-            return true;
-        }
-    }
-
-    // Check whether it is a goal or not
-
+  /*
+    Check whether it is a goal or not
+     */
     private static boolean isShot(int x, int y, char[][] board) {
         return board[x][y] == 'A';
     }
-    //input the ships of user's board
-
-    public static void inputUser() {
-
-        int n = 1;
-        print(user);
-        while (n < 7) {
-
-            System.out.println("Please, write down the coordinates if the start point of your ship('0 A'): ");
-            String coorN = scn.next();
-            String coorL = scn.next();
-            int x = coorN.charAt(0) - 48;
-            int y = coorL.charAt(0) - 65;
-            System.out.println(x + " " + y);
-            System.out.print("\nPlease, write down the direction of your ship\n" +
-                    "R - right\n" +
-                    "L - left\n" +
-                    "T - top\n" +
-                    "B - bottom\n");
-            char dir = scn.next().charAt(0);
-            if (checkerShip(x, y, dir, n, user)) {
-                n++;
-            } else
-                System.out.println("Try again, it seems error coordinates :(");
-            print(user);
-        }
-        System.out.println("LET'S START");
-    }
-
-    // check whether the ship on the right place on the board
+    /*
+check whether the ship on the right place on the board
+ */
     private static boolean checkerShip(int x, int y, char dir, int n, char[][] board) {
         int sign = 0;
         int os = -1;
@@ -164,7 +106,7 @@ public class Game {
                     os = x;
                     break;
             }
-            if (os + (sign * ship) < 0 || os + (sign * ship) > 9) {
+            if (os + (sign * ship) < -1 || os + (sign * ship) > 10) {
                 return false;
             }
             for (int i = os; sign == 1 ? i < os + (sign * ship) : i > os + (sign * ship); i += sign) {
@@ -187,7 +129,10 @@ public class Game {
         return stop == 0;
     }
 
-    // the Set of ships of Computer's board
+
+        /*
+    the Set of ships of Computer's board
+     */
 
     public static void setCompShips() {
         int n = 1;
@@ -202,7 +147,9 @@ public class Game {
         }
     }
 
-    //the Hit of the Computer
+    /*
+    the Hit of the Computer
+     */
     public static boolean hitOfOp() {
 
         int x = (int) (Math.random() * (MAX - 1));
@@ -210,16 +157,88 @@ public class Game {
         if (isShot(x, y, user)) {
             UserShips--;
             user[x][y] = 'X';
+
             System.out.println("Opponent got!");
             return true;
         } else if (user[x][y] == 'o') {
             user[x][y] = 'M';
+
             System.out.println("Opponent missed");
             return false;
         } else
             return true;
     }
+    /*
+    Input the coordinates of the hit by User
+     */
+    public static boolean inputHit() {
+        System.out.print("\nPlease, write down the coordinates of the hit ('0 A'): ");
+        String coorN = scn.next();
+        String coorL = scn.next();
+        int x = coorN.charAt(0) - 48;
+        int y = coorL.charAt(0) - 65;
+        try {
+            if (isShot(x, y, op)) {
+                op[x][y] = 'X';
+                copy[x][y] = 'X';
+                CompShips--;
+                System.out.println("You got it! Hit again");
+                return true;
 
+            } else if (op[x][y] == 'o') {
+                System.out.println("You missed :(");
+                copy[x][y] = 'M';
+                return false;
+            } else return true;
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("The out of board! Try again!");
+            return true;
+        }
+    }
+
+
+    //input the ships of user's board
+
+    public static void inputUser() {
+
+        int n = 1;
+        print(user);
+        while (n < 7) {
+
+            System.out.println("Please, write down the coordinates if the start point of your ship('0 A'): ");
+            String coorN = scn.next();
+            String coorL = scn.next();
+            int x = coorN.charAt(0) - 48;
+            int y = coorL.charAt(0) - 65;
+            System.out.println(x + " " + y);
+            System.out.print("\nPlease, write down the direction of your ship\n" +
+                    "R - right\n" +
+                    "L - left\n" +
+                    "T - top\n" +
+                    "B - bottom\n");
+            char dir = scn.next().charAt(0);
+            if (checkerShip(x, y, dir, n, user)) {
+                n++;
+            } else
+                System.out.println("Try again, it seems error coordinates :(");
+            print(user);
+        }
+        System.out.println("LET'S START");
+    }
+
+    /*
+    Printing the board
+     */
+    private static void print(char[][] board) {
+        for (int i = 0; i < MAX; i++) {
+            System.out.printf(" %d", i);
+            for (int j = 0; j < MAX; j++) {
+                System.out.print(" " + board[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.print("-- A B C D E F G H I J\n");
+    }
     public static void printAll() {
         System.out.println("~~~~~~~~~~~~~~~THE BATTLESHIPS~~~~~~~~~~~~~~~~");
         print(user);
@@ -228,7 +247,9 @@ public class Game {
         print(copy);
     }
 
-    //The wait method
+    /*
+    The wait method
+     */
 
     public static void wait(int sec) {
         try {
